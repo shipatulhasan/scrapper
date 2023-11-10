2682a113471086a7d891a699d99b90d100ff76d6
shipatulhasan <shipatulhasan328@gmail.com>
2023-11-10 15:13:06 +0600

    update all row

--- index.js
+++ index.js
@@ -23,13 +23,54 @@ async function scrapeData() {
       const scrapedData = []
 
       rows.forEach((row) => {
-        const name = row.querySelector('.tv-screener__symbol').innerText.trim()
+        const name = row.querySelector('.tv-screener__symbol')?.innerText.trim()
         const close = row
           .querySelector('[data-field-key="close"] span')
-          .innerText.trim()
+          ?.innerText.trim()
         // ... extract other fields similarly
+        const change = row
+          .querySelector('[data-field-key="change"]')
+          ?.innerText.trim()
+        const changeAbs = row
+          .querySelector('[data-field-key="change_abs"]')
+          ?.innerText.trim()
+        const high = row
+          .querySelector('[data-field-key="high"]')
+          ?.innerText.trim()
+        const low = row
+          .querySelector('[data-field-key="low"]')
+          ?.innerText.trim()
+        const volume = row
+          .querySelector('[data-field-key="volume"]')
+          ?.innerText.trim()
+        const vol24h = row
+          .querySelector('[data-field-key="24h_vol|5"]')
+          ?.innerText.trim()
+        const vol24hChange = row
+          .querySelector('[data-field-key="24h_vol_change|5"]')
+          ?.innerText.trim()
+        const signal = row
+          .querySelector(
+            '[data-field-key="Recommend.All"] .tv-screener-table__signal'
+          )
+          ?.innerText.trim()
+        const exchange = row
+          .querySelector('[data-field-key="exchange"]')
+          ?.innerText.trim()
 
-        scrapedData.push({ name, close /*, ... other fields */ })
+        scrapedData.push({
+          name,
+          close,
+          change,
+          changeAbs,
+          high,
+          low,
+          volume,
+          vol24h,
+          vol24hChange,
+          signal,
+          exchange
+        })
       })
 
       return scrapedData
